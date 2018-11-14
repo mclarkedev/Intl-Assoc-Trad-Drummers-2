b1062e1d6524a48ebc8ee118945a77a90f3feeee
Megan Clarke <meganclarkedev@gmail.com>
2018-11-13 19:20:05 -0600

    Initial Commit

---
+++ script.js
@@ -0,0 +1,8 @@
+/* Nav Burger */
+let mainNav = document.getElementById('js-menu');
+let navBarToggle = document.getElementById('js-navbar-toggle');
+
+navBarToggle.addEventListener('click', function () {
+    mainNav.classList.toggle('main-nav-active');
+});
+
